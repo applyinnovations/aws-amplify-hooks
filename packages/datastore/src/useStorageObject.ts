7f5a2307e871f1b03db1c7779c87716c64d05437
Alexander Bunn <alexander@applyinnovations.com.au>
2022-03-10 13:02:00 +1000

    added error and loading states to file url

--- packages/datastore/src/useStorageObject.ts
+++ packages/datastore/src/useStorageObject.ts
@@ -5,10 +5,21 @@ import { StorageObject } from './types';
 export const useStorageObject = (storageObject?: StorageObject | null) => {
   const [fileUrl, setFileUrl] = useState<string>();
   const [mounted, setMounted] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   useEffect(() => {
-    if (storageObject)
-      getFileUrl(storageObject).then((url) => mounted && setFileUrl(url));
+    if (storageObject) {
+      setLoading(true);
+      getFileUrl(storageObject)
+        .then((url) => mounted && setFileUrl(url))
+        .catch((e) => mounted && setError(e.message))
+        .finally(() => mounted && setLoading(false));
+    }
     return () => setMounted(false);
   }, [storageObject]);
-  return fileUrl;
+  return {
+    url: fileUrl,
+    loading: loading,
+    error: error,
+  };
 };
