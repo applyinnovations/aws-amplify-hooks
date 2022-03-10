69f679d3512137781b17819f258c52802d740975
Alexander Bunn <alexander@applyinnovations.com.au>
2022-03-10 13:30:50 +1000

    updates

--- packages/datastore/src/useStorageObject.ts
+++ packages/datastore/src/useStorageObject.ts
@@ -11,14 +11,33 @@ export const useStorageObject = (storageObject?: StorageObject | null) => {
   useEffect(() => {
     console.log('storage object updated');
     if (storageObject) {
+      console.log('valid storage object', storageObject);
       setLoading(true);
       getFileUrl(storageObject)
-        .then((url) => mounted && setUrl(url))
-        .catch((e) => mounted && setError(e.message))
-        .finally(() => mounted && setLoading(false));
+        .then((url) => {
+          if (mounted) {
+            console.log('got url', url);
+            setUrl(url);
+            setError(undefined);
+          }
+        })
+        .catch((e: Error) => {
+          if (mounted) {
+            console.log('got error', e);
+            setUrl(undefined);
+            setError(e.message);
+          }
+        })
+        .finally(() => {
+          if (mounted) {
+            setLoading(false);
+            console.log('finished');
+          }
+        });
     } else {
+      console.log('Invalid storage object', storageObject);
       setLoading(false);
-      setError('No image found.');
+      setError('Missing storage object');
       setUrl(undefined);
     }
     return () => setMounted(false);
