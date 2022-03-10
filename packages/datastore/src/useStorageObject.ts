c6c2859734fd1b435e9af111711ee22ea2a4901e
Alexander Bunn <alexander@applyinnovations.com.au>
2022-03-10 13:39:46 +1000

    updates

--- packages/datastore/src/useStorageObject.ts
+++ packages/datastore/src/useStorageObject.ts
@@ -7,41 +7,30 @@ export const useStorageObject = (storageObject?: StorageObject | null) => {
   const [mounted, setMounted] = useState(true);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string>();
-  const serialisedStorageObject = JSON.stringify(storageObject);
+  useEffect(() => () => setMounted(false), []);
   useEffect(() => {
-    console.log('storage object updated');
     if (storageObject) {
-      console.log('valid storage object', storageObject);
       setLoading(true);
       getFileUrl(storageObject)
         .then((url) => {
           if (mounted) {
-            console.log('got url', url);
             setUrl(url);
             setError(undefined);
           }
         })
         .catch((e: Error) => {
           if (mounted) {
-            console.log('got error', e);
             setUrl(undefined);
             setError(e.message);
           }
         })
-        .finally(() => {
-          if (mounted) {
-            setLoading(false);
-            console.log('finished');
-          }
-        });
+        .finally(() => mounted && setLoading(false));
     } else {
-      console.log('Invalid storage object', storageObject);
       setLoading(false);
       setError('Missing storage object');
       setUrl(undefined);
     }
-    return () => setMounted(false);
-  }, [serialisedStorageObject]);
+  }, [storageObject]);
   return {
     url,
     loading,
