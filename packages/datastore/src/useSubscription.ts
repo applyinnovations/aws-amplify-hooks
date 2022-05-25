24b5702a81e650370a46488a47eae1ccd2fc98fe
Peter Allan Maala David <peterallanmaaladavid@Peters-MacBook-Air.local>
2022-05-25 15:11:59 +0800

    change how loading state is being resolved

--- packages/datastore/src/useSubscription.ts
+++ packages/datastore/src/useSubscription.ts
@@ -5,9 +5,9 @@
   PersistentModel,
   PersistentModelConstructor,
   ProducerModelPredicate,
-} from '@aws-amplify/datastore';
-import { PredicateAll } from '@aws-amplify/datastore/lib-esm/predicates';
-import { useState, useEffect, useCallback } from 'react';
+} from "@aws-amplify/datastore";
+import { PredicateAll } from "@aws-amplify/datastore/lib-esm/predicates";
+import { useState, useEffect, useCallback, useMemo } from "react";
 
 export { PredicateAll };
 
@@ -19,32 +19,31 @@ export function useSubscription<T extends PersistentModel>({
   onError,
 }: {
   model: PersistentModelConstructor<T>;
-  id?: T['id'];
+  id?: T["id"];
   criteria?: ProducerModelPredicate<T> | typeof PredicateAll;
   paginationProducer?: ObserveQueryOptions<T>;
   onError?: (error: any) => void;
 }) {
-  const [data, setData] = useState<DataStoreSnapshot<T>['items']>();
+  const [data, setData] = useState<DataStoreSnapshot<T>["items"]>();
   const [error, setError] = useState<any>();
-  const [loading, setLoading] = useState(false);
+
   const [spamCount, setSpamCount] = useState(0);
   const [startTime, setStartTime] = useState(performance.now());
 
   if (id && criteria)
-    throw Error('Please provide only `id` or `criteria` not both');
+    throw Error("Please provide only `id` or `criteria` not both");
 
   const idCriteria: ProducerModelPredicate<T> | undefined = useCallback(
-    (d) => (id ? d.id('eq', id) : undefined),
+    (d) => (id ? d.id("eq", id) : undefined),
     [id]
   );
 
   useEffect(() => {
-    setLoading(true);
     const elapsedTime = performance.now() - startTime;
     if (spamCount > 25 && spamCount / elapsedTime > 0.01)
       throw Error(
-        'The props for useSubscription are being updated too fast. ' +
-          'Please use `useCallback` or `useMemo` on props to fix performance issues.'
+        "The props for useSubscription are being updated too fast. " +
+          "Please use `useCallback` or `useMemo` on props to fix performance issues."
       );
     else {
       setSpamCount((c) => c + 1);
@@ -57,18 +56,20 @@ export function useSubscription<T extends PersistentModel>({
           const data = msg.items;
           setData(data);
           setError(undefined);
-          setLoading(false);
         },
         (error) => {
           setError(error);
           if (onError) onError(error);
           console.error(error);
-          setLoading(false);
         }
       );
       return () => sub.unsubscribe();
     }
   }, [model, idCriteria, criteria, paginationProducer, onError]);
+
+  const loading = useMemo(() => {
+    return Boolean(!data?.length);
+  }, [data]);
 
   return {
     first: data?.[0],
